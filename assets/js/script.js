5d4687ac28238856b15a14c1fb2d3b1073c3d037
Jonathan Wilferd <jwilferd10@yahoo.com>
2023-10-15 00:01:17 -0700

    Adjusted the script.js, now using querySelectorAll for displayCard and running a forEach loop to collect each img and gif for every entry

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,24 @@
-// querySelectors
-const displayCard = document.querySelector('.displayCard');
-const projectImg = displayCard.querySelector('.projectImg');
-const projectGif = displayCard.querySelector('.projectGif');
+// Get all .displayCard elements on the page
+const displayCards = document.querySelectorAll('.displayCard');
 
-// addEventListeners for mouseover and mouseout
+// Iterate through each card
+displayCards.forEach((displayCard) => {
+    const projectImg = displayCard.querySelector('.projectImg');
+    const projectGif = displayCard.querySelector('.projectGif');
+    // Store the initial GIF source for each card
+    const initialGifSrc = projectGif.src;
 
-// Play the gif on mouseover
-displayCard.addEventListener('mouseover', () => {
-    projectImg.style.display = 'none';
-    projectGif.style.display = 'block';
-    projectGif.src = 'animated.gif';
-});
+    // Play the gif on mouseover
+    displayCard.addEventListener('mouseover', () => {
+        projectImg.style.display = 'none';
+        projectGif.style.display = 'block';
+        projectGif.src = initialGifSrc;
+    });
 
-// Pause the GIF on moouseout
-displayCard.addEventListener('mouseout', () => {
-    projectGif.style.display = 'none';
-    projectImg.style.display = 'block';
-    projectGif.src = '';
+    // Pause and reset the GIF on mouseout
+    displayCard.addEventListener('mouseout', () => {
+        projectGif.style.display = 'none';
+        projectImg.style.display = 'block';
+        projectGif.src = '';
+    });
 });
