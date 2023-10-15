6053f802c3e79c9ae69e6303324e029806cdbffe
Jonathan Wilferd <jwilferd10@yahoo.com>
2023-10-14 23:52:34 -0700

    Added the mouseover and mouseout effect. Noticed a bug, but this is working and is a checkpoint

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
 // querySelectors
+const displayCard = document.querySelector('.displayCard');
+const projectImg = displayCard.querySelector('.projectImg');
+const projectGif = displayCard.querySelector('.projectGif');
 
 // addEventListeners for mouseover and mouseout
-    // Play the gif on mouseover
 
-    // Pause the GIF on moouseout
+// Play the gif on mouseover
+displayCard.addEventListener('mouseover', () => {
+    projectImg.style.display = 'none';
+    projectGif.style.display = 'block';
+    projectGif.src = 'animated.gif';
+});
+
+// Pause the GIF on moouseout
+displayCard.addEventListener('mouseout', () => {
+    projectGif.style.display = 'none';
+    projectImg.style.display = 'block';
+    projectGif.src = '';
+});
