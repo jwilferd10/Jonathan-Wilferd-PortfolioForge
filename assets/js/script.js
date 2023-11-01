7fce28e23047f619cb70fccc9fcb590950f491da
Jonathan Wilferd <jwilferd10@yahoo.com>
2023-11-01 10:39:52 -0700

    Created a new buffer area between header and main content. Additionally created a 'click me' button that brings users to the About Me section. Used JavaScript to create a smooth transition

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,3 +30,28 @@ displayCards.forEach((displayCard) => {
         } 
     });
 });
+
+// Select all elements with the 'scroll' class and attach an event listener for each of them.
+document.querySelectorAll('.scroll').forEach(anchor => {
+    anchor.addEventListener('click', function(event) {
+        // Prevent the default jump behavior
+        event.preventDefault();
+
+        // Get the ID of the target section from the 'href' attribute of the link clicked
+        const targetID = this.getAttribute('href');
+
+        // Find the target section in the document using that ID
+        const targetSection = document.querySelector(targetID);
+
+        if (targetSection) {
+            // If the target section exists, get the distance
+            const offsetTop = targetSection.offsetTop;
+
+            // Smoothly scroll to the targetSection
+            window.scrollTo({
+                top: offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
