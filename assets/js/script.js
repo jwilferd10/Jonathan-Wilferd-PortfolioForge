365801e9d24d536b9027da2588704af4e2d77b64
Jonathan Wilferd <jwilferd10@yahoo.com>
2023-11-05 13:27:03 -0800

    Adjusted the observerCallback to look for skillsSectionEl and run two new animations for the specific section

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,6 +60,8 @@ document.querySelectorAll('.scroll').forEach(anchor => {
 const aboutDiv1El = document.getElementById('aboutDiv1');
 const achievementsEl = document.getElementById('achievements');
 const aboutDiv2El = document.getElementById('aboutDiv2');
+const skillsListEl = document.getElementById('skillsList');
+const skillsNotificationEl = document.getElementById('skillsNotification');
 
 // Set the options for the Intersection Observer 
 let options = {
@@ -74,15 +76,21 @@ const observerCallback = (entries, observer) => {
     entries.forEach(entry => {
         // If the section is truthy, apply the CSS animations
         if (entry.isIntersecting) {
-            // Remove the hidden class from each element
-            aboutDiv1El.classList.remove('hidden');
-            achievementsEl.classList.remove('hidden');
-            aboutDiv2El.classList.remove('hidden');
+            if (entry.target.id === 'aboutSection') {
+                // Remove the hidden class from each element
+                aboutDiv1El.classList.remove('hidden');
+                achievementsEl.classList.remove('hidden');
+                aboutDiv2El.classList.remove('hidden');
 
-            // Apply animations when in view
-            aboutDiv1El.style.animation = 'fadeIntoView 1.8s forwards';
-            achievementsEl.style.animation = 'slideBottomToTop 1.8s forwards';
-            aboutDiv2El.style.animation = 'slideRightToLeft 1.8s forwards';
+                // Apply animations when in view
+                aboutDiv1El.style.animation = 'fadeIntoView 1.8s forwards';
+                achievementsEl.style.animation = 'slideBottomToTop 1.8s forwards';
+                aboutDiv2El.style.animation = 'slideRightToLeft 1.8s forwards';
+            } else if (entry.target.id ==='skillsSection') {
+
+                skillsListEl.style.animation = 'slideRightToLeft 2s forwards';
+                skillsNotificationEl.style.animation = 'fadeIntoView 5s forwards';
+            }
 
             // Use unobserve to prevent re-triggering
             observer.unobserve(entry.target);
@@ -95,4 +103,6 @@ const observer = new IntersectionObserver(observerCallback, options);
 
 // Observe the about section
 const aboutSectionEl = document.getElementById('aboutSection');
+const skillsSectionEl = document.getElementById('skillsSection');
 observer.observe(aboutSectionEl);
+observer.observe(skillsSectionEl);
