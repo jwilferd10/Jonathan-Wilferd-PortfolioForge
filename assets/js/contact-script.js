2ea9160e90b92f39ab4f1a21c2ba4f14a8e0b023
Jonathan Wilferd <jwilferd10@yahoo.com>
2023-11-09 09:23:17 -0800

    Fixed script link, set up an event listener to test. eventListener will stay as it will be used further on

--- assets/js/contact-script.js
+++ assets/js/contact-script.js
@@ -1,3 +1,8 @@
 const nameInputEl = document.getElementById('nameInput');
 const emailAddressEl = document.getElementById('nameInput');
 const formMessageEl = document.getElementById('nameInput');
+const submitBtnEl = document.getElementById('submitBtn');
+
+submitBtnEl.addEventListener('click', () => {
+    console.log("Submit button clicked");
+})
