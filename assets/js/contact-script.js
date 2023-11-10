d534260e017bf672ca9770be40b11f82e64bcfb4
Jonathan Wilferd <jwilferd10@yahoo.com>
2023-11-09 23:32:51 -0800

    Renamed 'test' to 'errorColor' and also added helpful comments for future self

--- assets/js/contact-script.js
+++ assets/js/contact-script.js
@@ -6,22 +6,26 @@ const submitBtnEl = document.getElementById('submitBtn');
 submitBtnEl.addEventListener('click', (event) => {
     event.preventDefault();
 
+    // Form Elements
     const formField = [nameInputEl, emailAddressEl, formMessageEl];
     let checkEmptyField = false;
 
+    // Check if each part of the form has a value inside
     formField.forEach(field => {
         if (field.value === '') {
             checkEmptyField = true;
         }
 
         if (checkEmptyField) {
+            // Add animations to the form
             field.classList.add('emptyFieldAnimation');
             field.style.animation = 'emptyFieldAnimation 2s';
-            field.classList.add('test');
+            field.classList.add('errorColor');
 
+            // Remove the class after timeout conditions trigger
             setTimeout(() => {
                 field.classList.remove('emptyFieldAnimation');
-                field.classList.remove('test');
+                field.classList.remove('errorColor');
                 field.style.animation = '';
             }, 1500);
         }
