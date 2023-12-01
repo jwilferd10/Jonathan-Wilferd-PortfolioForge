5e6197263c1f63f531192c1df77becb83ce16cd7
Jonathan Wilferd <jwilferd10@yahoo.com>
2023-11-30 22:31:44 -0800

    Created a showNavigation function, renamed closeHamburger to closeNavigation. Called each function where necessary

--- assets/js/hamburger.js
+++ assets/js/hamburger.js
@@ -4,20 +4,26 @@ const mobileNavEl = document.getElementById('mobileNav');
 const headerEl = document.getElementById('headerSection');
 const nameHeaderEl = document.getElementById('nameHeader');
 
-const closeHamburger = () => {
+// Function that hides the mobile hamburger display
+const closeNavigation = () => {
     mobileNavEl.style.display = 'none';
     hamburgerEl.style.color = '#333333'
     headerEl.style.backgroundColor = '';
     nameHeaderEl.style.backgroundColor = '#333333'
 }
 
+// Function that showcases the hamburger display
+const showNavigation = () => {
+    mobileNavEl.style.display = 'block';
+    hamburgerEl.style.color = '#c0c0c0';
+    headerEl.style.backgroundColor = '#333333';
+    nameHeaderEl.style.backgroundColor = '#195687';
+}
+
 hamburgerEl.addEventListener('click', () => {
     if (mobileNavEl.style.display === 'block') {
-        closeHamburger();
+        closeNavigation();
     } else {
-        mobileNavEl.style.display = 'block';
-        hamburgerEl.style.color = '#c0c0c0';
-        headerEl.style.backgroundColor = '#333333';
-        nameHeaderEl.style.backgroundColor = '#195687';
+        showNavigation();
     }
 })
