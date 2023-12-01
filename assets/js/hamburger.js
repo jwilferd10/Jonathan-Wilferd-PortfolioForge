4cd5684088f6e1cc745e3956bfec91e40c33664c
Jonathan Wilferd <jwilferd10@yahoo.com>
2023-11-30 23:02:29 -0800

    Created a new event listener that checks where the user is clicking. Additionally applied event.stopPropagation to allow the body.eventListener to operate

--- assets/js/hamburger.js
+++ assets/js/hamburger.js
@@ -10,7 +10,7 @@ const closeNavigation = () => {
     hamburgerEl.style.color = '#333333'
     headerEl.style.backgroundColor = '';
     nameHeaderEl.style.backgroundColor = '#333333'
-}
+};
 
 // Function that showcases the hamburger display
 const showNavigation = () => {
@@ -18,12 +18,24 @@ const showNavigation = () => {
     hamburgerEl.style.color = '#c0c0c0';
     headerEl.style.backgroundColor = '#333333';
     nameHeaderEl.style.backgroundColor = '#195687';
-}
+};
 
-hamburgerEl.addEventListener('click', () => {
+// Toggle to show or hide mobile navigation links
+hamburgerEl.addEventListener('click', (event) => {
+    event.stopPropagation();
     if (mobileNavEl.style.display === 'block') {
         closeNavigation();
     } else {
         showNavigation();
     }
-})
+});
+
+// Close mobile navigation when clicking outside the hamburger or mobile navigation
+document.body.addEventListener('click', (event) => {
+    const isHamburgerClick = event.target === hamburgerEl;
+    const isMobileNavClick = event.target.closest('#mobileNav') !== null;
+
+    if (!isHamburgerClick && !isMobileNavClick) {
+        closeNavigation();
+    }
+});
