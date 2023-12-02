fc8a82f8d9957c23e293f7b4caba28663ed14ebb
Jonathan Wilferd <jwilferd10@yahoo.com>
2023-12-02 07:48:19 -0800

    Removed commented out code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,3 @@
-// // Get all .displayCard elements on the page
-// const displayCards = document.querySelectorAll('.displayCard');
-
-// const preloadGif = (gifSrc, callback) => {
-//     const img = new Image();
-//     img.onload = callback;
-//     img.src = gifSrc;
-// };
-
-// // Iterate through each card
-// displayCards.forEach((displayCard) => {
-//     const projectGif = displayCard.querySelector('.projectGif');
-    
-//     // Store the initial GIF source for each card
-//     const initialGifSrc = projectGif.src;
-
-//     // Set a variable to track if the GIF is loaded
-//     let gifLoaded = false;
-
-//     // Preload the GIF
-//     preloadGif(initialGifSrc, () => {
-//         gifLoaded = true;
-//     });
-
-//     // Play the gif on mouseover
-//     displayCard.addEventListener('mouseenter', () => {
-//         // Check if the GIF is already loaded, switch on if true
-//         if (gifLoaded) {
-//             projectGif.src = initialGifSrc;
-//         } 
-//     });
-// });
-
 // Select all elements with the 'scroll' class and attach an event listener for each of them.
 document.querySelectorAll('.scroll').forEach(anchor => {
     anchor.addEventListener('click', function(event) {
