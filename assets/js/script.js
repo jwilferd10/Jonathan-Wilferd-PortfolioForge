e7d820557bd0be3ca25088dfdae02cbbea44c29f
Jonathan Wilferd <jwilferd10@yahoo.com>
2023-12-02 21:04:48 -0800

    Removed code from script.js that adjusted scroll-behavior and just used CSS' scroll-behavior: smooth.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,9 @@
-// Select all elements with the 'scroll' class and attach an event listener for each of them.
-document.querySelectorAll('.scroll').forEach(anchor => {
-    anchor.addEventListener('click', function(event) {
-        // Prevent the default jump behavior
-        event.preventDefault();
-
-        // Get the ID of the target section from the 'href' attribute of the link clicked
-        const targetID = this.getAttribute('href');
-
-        // Find the target section in the document using that ID
-        const targetSection = document.querySelector(targetID);
-
-        if (targetSection) {
-            // If the target section exists, get the distance
-            const offsetTop = targetSection.offsetTop;
-
-            // Smoothly scroll to the targetSection
-            window.scrollTo({
-                top: offsetTop,
-                behavior: 'smooth'
-            });
-        }
-    });
-});
-
 // Define the HTML elements to be animated
 const aboutDiv1El = document.getElementById('aboutDiv1');
 const achievementsEl = document.getElementById('achievements');
 const aboutDiv2El = document.getElementById('aboutDiv2');
 const skillsListEl = document.getElementById('skillsList');
 const skillsNotificationEl = document.getElementById('skillsNotification');
-
 const skillsListWrapperEl = document.getElementById('skillsListWrapper');
 
 // Set the options for the Intersection Observer 
