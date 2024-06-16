dcb0dad58a5e4fcd10f36d9fab869164f1d56545
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-06-15 21:54:43 -0700

    Commented out the contact-script.js file. This is a checkpoint, the file's being removed as it is now redundant.

--- assets/js/contact-script.js
+++ assets/js/contact-script.js
@@ -1,43 +1,43 @@
-const nameInputEl = document.getElementById('nameInput');
-const emailAddressEl = document.getElementById('emailAddress');
-const formMessageEl = document.getElementById('formMessage');
-const submitBtnEl = document.getElementById('submitBtn');
+// const nameInputEl = document.getElementById('nameInput');
+// const emailAddressEl = document.getElementById('emailAddress');
+// const formMessageEl = document.getElementById('formMessage');
+// const submitBtnEl = document.getElementById('submitBtn');
 
-submitBtnEl.addEventListener('click', (event) => {
-    event.preventDefault();
+// submitBtnEl.addEventListener('click', (event) => {
+//     event.preventDefault();
 
-    // Form Elements
-    const formField = [nameInputEl, emailAddressEl, formMessageEl];
-    let areFieldsEmpty = false;
+//     // Form Elements
+//     const formField = [nameInputEl, emailAddressEl, formMessageEl];
+//     let areFieldsEmpty = false;
 
-    // Check if each part of the form has a value inside
-    formField.forEach(field => {
-        if (field.value === '') {
-            areFieldsEmpty = true;
-        }
+//     // Check if each part of the form has a value inside
+//     formField.forEach(field => {
+//         if (field.value === '') {
+//             areFieldsEmpty = true;
+//         }
 
-        if (areFieldsEmpty) {
-            // Add animations to the form
-            field.classList.add('fieldAnimation');
-            field.style.animation = 'fieldAnimation 2s';
-            field.classList.add('errorColor');
+//         if (areFieldsEmpty) {
+//             // Add animations to the form
+//             field.classList.add('fieldAnimation');
+//             field.style.animation = 'fieldAnimation 2s';
+//             field.classList.add('errorColor');
 
-            // Remove the class after timeout conditions trigger
-            setTimeout(() => {
-                field.classList.remove('fieldAnimation');
-                field.classList.remove('errorColor');
-                field.style.animation = '';
-            }, 1500);
-        } else if (!areFieldsEmpty) {
-            field.classList.add('successColor');
-            field.style.animation = 'fieldAnimation 2s';
+//             // Remove the class after timeout conditions trigger
+//             setTimeout(() => {
+//                 field.classList.remove('fieldAnimation');
+//                 field.classList.remove('errorColor');
+//                 field.style.animation = '';
+//             }, 1500);
+//         } else if (!areFieldsEmpty) {
+//             field.classList.add('successColor');
+//             field.style.animation = 'fieldAnimation 2s';
 
-            // Remove the class after timeout conditions trigger
-            setTimeout(() => {
-                field.classList.remove('fieldAnimation');
-                field.classList.remove('successColor');
-                field.style.animation = '';
-            }, 1500);
-        }
-    });
-});
+//             // Remove the class after timeout conditions trigger
+//             setTimeout(() => {
+//                 field.classList.remove('fieldAnimation');
+//                 field.classList.remove('successColor');
+//                 field.style.animation = '';
+//             }, 1500);
+//         }
+//     });
+// });
