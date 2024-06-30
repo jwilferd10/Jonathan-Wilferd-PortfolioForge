b90c817e69c33e56d831e7f9a3eff26a66eaf5b3
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-06-30 09:38:12 -0700

    Updated themeToggle to use try/catch to handle for any errors

--- assets/js/themeToggle.js
+++ assets/js/themeToggle.js
@@ -4,26 +4,31 @@ const emojiToggleEl = document.querySelector('.emojiToggle');
 
 // Toggle between light and dark themes 
 const toggleTheme = () => {
-    let currentPath = window.location.pathname
-    let switchColors;
+    try {
+        let currentPath = window.location.pathname
+        let switchColors;
 
-    let initialTheme = currentThemeEl.getAttribute('href').includes('blue-silver.css');
+        let initialTheme = currentThemeEl.getAttribute('href').includes('blue-silver.css');
 
-    if (currentPath.includes('/html/')) {
-        // Current path is within the html directory
-        switchColors = initialTheme ? '../assets/css/dark-mode.css' : '../assets/css/blue-silver.css';
-    } else {
-        // Current path is the root path
-        switchColors = initialTheme ? 'assets/css/dark-mode.css' : 'assets/css/blue-silver.css';
+        if (currentPath.includes('/html/')) {
+            // Current path is within the html directory
+            switchColors = initialTheme ? '../assets/css/dark-mode.css' : '../assets/css/blue-silver.css';
+        } else {
+            // Current path is the root path
+            switchColors = initialTheme ? 'assets/css/dark-mode.css' : 'assets/css/blue-silver.css';
+        }
+
+        // Change the href attribute to switch the CSS file
+        currentThemeEl.setAttribute('href', switchColors);
+
+        // Toggle the emoji icon
+        const emojiClasses = 'emojiToggle fas fa-2x'
+        let switchEmoji = emojiToggleEl.getAttribute('class').includes('fa-sun') ? `${emojiClasses} fa-moon` : `${emojiClasses} fa-sun`;
+        emojiToggleEl.setAttribute('class', switchEmoji);
+    } catch (error) {
+        console.log('An error has occurred while trying to toggle the theme:', error);
+        window.alert('Something when wrong while trying to toggle the theme');
     }
-
-    // Change the href attribute to switch the CSS file
-    currentThemeEl.setAttribute('href', switchColors);
-
-    // Toggle the emoji icon
-    const emojiClasses = 'emojiToggle fas fa-2x'
-    let switchEmoji = emojiToggleEl.getAttribute('class').includes('fa-sun') ? `${emojiClasses} fa-moon` : `${emojiClasses} fa-sun`;
-    emojiToggleEl.setAttribute('class', switchEmoji);
 }
 
 toggleButtonEl.forEach(button => {
