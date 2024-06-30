7f5ec1f02a7baf5c85990ec39f326b38a8225e9a
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-06-29 21:59:29 -0700

    Refactored themeToggle.js to check location.pathname, now accounting for if the pathname includes /html/.

--- assets/js/themeToggle.js
+++ assets/js/themeToggle.js
@@ -2,15 +2,24 @@ const currentThemeEl = document.getElementById('currentTheme');
 const toggleButtonEl = document.querySelectorAll('.toggleButton');
 const emojiToggleEl = document.querySelector('.emojiToggle');
 
+// Toggle between light and dark themes 
 const toggleTheme = () => {
-    // Toggle between light and dark themes 
-    let switchColors = currentThemeEl.getAttribute('href').includes('blue-silver.css') ? 'assets/css/dark-mode.css' : 'assets/css/blue-silver.css';
+    let currentPath = window.location.pathname
+    let switchColors;
+
+    if (currentPath.includes('/html/')) {
+        // Current path is within the html directory
+        switchColors = currentThemeEl.getAttribute('href').includes('blue-silver.css') ? '../assets/css/dark-mode.css' : '../assets/css/blue-silver.css';
+    } else {
+        // Current path is the root path
+        switchColors = currentThemeEl.getAttribute('href').includes('blue-silver.css') ? 'assets/css/dark-mode.css' : 'assets/css/blue-silver.css';
+    }
 
     // Change the href attribute to switch the CSS file
     currentThemeEl.setAttribute('href', switchColors);
 
+    // Toggle the emoji icon
     let switchEmoji = emojiToggleEl.getAttribute('class').includes('fa-sun') ? 'emojiToggle fas fa-moon fa-2x' : 'emojiToggle fas fa-sun fa-2x';
-
     emojiToggleEl.setAttribute('class', switchEmoji);
 }
 
